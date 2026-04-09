22eb1a760dcc4422b556543eded9b1f629170a11
unknown <rachitkumarsingh144@gmail.com>
2026-04-09 09:41:21 +0530

    subs plan fixed

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -9,7 +9,7 @@ const api = axios.create({
     headers: {
         'Content-Type': 'application/json',
     },
-    withCredentials: true, // Required for cookies/sessions across subdomains
+    withCredentials: false,
 });
 
 
